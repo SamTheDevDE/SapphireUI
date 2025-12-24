b85dc895c5432fd220dfb03e0fccac4d24eaf009
Samuel Domke <120846365+SamTheDevDE@users.noreply.github.com>
2025-12-24 18:13:57 +0100

    refactor the whole project

---
+++ lua/SapphireUI/init.lua
@@ -0,0 +1,9 @@
+-- SapphireUI/lua/SapphireUI/init.lua
+
+local SapphireUI = {}
+
+function SapphireUI.init(terminal)
+    -- Initialization logic here
+end
+
+return SapphireUI
--- startup.lua
+++
@@ -1,5 +0,0 @@
--- This file is the entry point for the SapphireUI examples.
--- By default, it runs the simple_window.lua example.
-
--- To run a different example, change the path below.
-shell.run("simple_window.lua")
